4a3cf70ad3c5bd70fe7ac2efaadb84a246ad068b
TukanDev <contact@tukandev.com>
2025-05-06 15:44:43 +0200

    Deprecate DOWNLOAD_MODE_FILE for hk4e and nap and introduce sophon parsing script
    Signed-off-by: TukanDev <contact@tukandev.com>

--- scripts/make_hoyo_manifests_sophon.js
+++ scripts/make_hoyo_manifests_sophon.js
@@ -14,10 +14,10 @@ let hsrfps = ["120"];
 let zzzfps = ["120", "144", "165", "180", "240"];
 let bh3fps = ["120"];
 
-let gipath = `${__dirname}/generated/hk4e_global.json`;
-let hsrpath = `${__dirname}/generated/hkrpg_global.json`;
-let zzzpath = `${__dirname}/generated/nap_global.json`;
-let bhpath = `${__dirname}/generated/bh3_global.json`;
+let gipath = `${__dirname}/generated_tests/hk4e_global.json`;
+let hsrpath = `${__dirname}/generated_tests/hkrpg_global.json`;
+let zzzpath = `${__dirname}/generated_tests/nap_global.json`;
+let bhpath = `${__dirname}/generated_tests/bh3_global.json`;
 
 async function queryHoyoPlayApis() {
     let rsp = await fetch(`${API}`);
@@ -72,9 +72,21 @@ async function queryHoyoPlayApis() {
        })
     });
 
+    let gb = r.data.game_branches;
+    let br = [];
+    gb.forEach((i) => {
+        br.push({
+            game_biz: i.game.biz,
+            game_id: i.game.id,
+            main: i.main,
+            preload: i.pre_download
+        })
+    })
+
     return {
         game_packages: pkgs,
         launch_configs: lce,
+        branches: br,
         assets: bgs
     };
 }
@@ -85,20 +97,20 @@ async function generateManifest(gameBiz) {
 
     let assets = rsp.assets.filter(e => e.game_biz === gameBiz)[0];
     let config = rsp.launch_configs.filter(e => e.game_biz === gameBiz)[0];
-    let packages = rsp.game_packages.filter(e => e.game_biz === gameBiz)[0];
+    let branches = rsp.branches.filter(e => e.game_biz === gameBiz)[0];
 
     let assetcfg = {game_icon: assets.icon, game_background: assets.background}
-    let pkg = formatPackages(packages);
+    let pkg = await formatPackages(branches);
 
     let final = {};
     switch (gameBiz) {
         case "hk4e_global": {
-            let metadatainfo = {versioned_name: `GenshinImpact ${packages.game_version} (Global)`, version: packages.game_version, game_hash: ""};
+            let metadatainfo = {versioned_name: `GenshinImpact ${branches.main.tag} (Global)`, version: branches.main.tag, game_hash: ""};
             let versioninfo = {
                 metadata: metadatainfo,
                 assets: assetcfg,
-                index_file: "",
-                res_list_url: packages.res_list,
+                index_file: `${pkg.manifest_base}`,
+                res_list_url: `${pkg.chunk_base}`,
                 game: {full: pkg.full_game, diff: pkg.diff_game},
                 audio: {full: pkg.full_audio, diff: pkg.diff_audio}
             };
@@ -112,7 +124,7 @@ async function generateManifest(gameBiz) {
                     gameversions.push(versioninfo);
 
                     data.game_versions.forEach(v => {
-                        if (v.metadata.version !== packages.game_version) {gameversions.push(v);}
+                        if (v.metadata.version !== branches.main.tag) {gameversions.push(v);}
                     });
                 } else {gameversions.push(versioninfo);}
             } else {gameversions.push(versioninfo);}
@@ -121,7 +133,7 @@ async function generateManifest(gameBiz) {
                 version: 1,
                 display_name: "GenshinImpact (Global)",
                 biz: "hk4e_global",
-                latest_version: packages.game_version,
+                latest_version: branches.main.tag,
                 game_versions: gameversions,
                 paths: {audio_pkg_res_dir: config.audio_pkg_res_dir, exe_filename: config.exe_filename, installation_dir: config.installation_dir, screenshot_dir: config.screenshot_dir, screenshot_dir_relative_to: "game_dir"},
                 assets: assetcfg,
@@ -133,18 +145,18 @@ async function generateManifest(gameBiz) {
                         jadeite: false,
                         xxmi: true
                     },
-                    preload: formatPreload(packages, "GenshinImpact")
+                    preload: await formatPreload(branches, "GenshinImpact")
                 }
             };
         }
         break;
         case "hkrpg_global": {
-            let metadatainfo = {versioned_name: `Honkai: StarRail ${packages.game_version} (Global)`, version: packages.game_version, game_hash: ""}
+            let metadatainfo = {versioned_name: `Honkai: StarRail ${branches.main.tag} (Global)`, version: branches.main.tag, game_hash: ""}
             let versioninfo = {
                 metadata: metadatainfo,
                 assets: assetcfg,
                 index_file: "",
-                res_list_url: packages.res_list,
+                res_list_url: "",
                 game: {full: pkg.full_game, diff: pkg.diff_game},
                 audio: {full: pkg.full_audio, diff: pkg.diff_audio}
             };
@@ -158,7 +170,7 @@ async function generateManifest(gameBiz) {
                     gameversions.push(versioninfo);
 
                     data.game_versions.forEach(v => {
-                        if (v.metadata.version !== packages.game_version) {gameversions.push(v);}
+                        if (v.metadata.version !== branches.main.tag) {gameversions.push(v);}
                     });
                 } else {gameversions.push(versioninfo);}
             } else {gameversions.push(versioninfo);}
@@ -167,7 +179,7 @@ async function generateManifest(gameBiz) {
                 version: 1,
                 display_name: "Honkai: StarRail (Global)",
                 biz: "hkrpg_global",
-                latest_version: packages.game_version,
+                latest_version: branches.main.tag,
                 game_versions: gameversions,
                 paths: {audio_pkg_res_dir: config.audio_pkg_res_dir, exe_filename: config.exe_filename, installation_dir: config.installation_dir, screenshot_dir: config.screenshot_dir, screenshot_dir_relative_to: "data_dir"},
                 assets: assetcfg,
@@ -179,18 +191,18 @@ async function generateManifest(gameBiz) {
                         jadeite: true,
                         xxmi: true
                     },
-                    preload: formatPreload(packages, "Honkai: StarRail")
+                    preload: await formatPreload(branches, "Honkai: StarRail")
                 }
             };
         }
         break;
         case "nap_global": {
-            let metadatainfo = {versioned_name: `ZenlessZoneZero ${packages.game_version} (Global)`, version: packages.game_version, game_hash: ""}
+            let metadatainfo = {versioned_name: `ZenlessZoneZero ${branches.main.tag} (Global)`, version: branches.main.tag, game_hash: ""}
             let versioninfo = {
                 metadata: metadatainfo,
                 assets: assetcfg,
-                index_file: "",
-                res_list_url: packages.res_list,
+                index_file: `${pkg.manifest_base}`,
+                res_list_url: `${pkg.chunk_base}`,
                 game: {full: pkg.full_game, diff: pkg.diff_game},
                 audio: {full: pkg.full_audio, diff: pkg.diff_audio}
             };
@@ -204,7 +216,7 @@ async function generateManifest(gameBiz) {
                     gameversions.push(versioninfo);
 
                     data.game_versions.forEach(v => {
-                        if (v.metadata.version !== packages.game_version) {gameversions.push(v);}
+                        if (v.metadata.version !== branches.main.tag) {gameversions.push(v);}
                     });
                 } else {gameversions.push(versioninfo);}
             } else {gameversions.push(versioninfo);}
@@ -213,7 +225,7 @@ async function generateManifest(gameBiz) {
                 version: 1,
                 display_name: "ZenlessZoneZero (Global)",
                 biz: "nap_global",
-                latest_version: packages.game_version,
+                latest_version: branches.main.tag,
                 game_versions: gameversions,
                 paths: {audio_pkg_res_dir: config.audio_pkg_res_dir, exe_filename: config.exe_filename, installation_dir: config.installation_dir, screenshot_dir: config.screenshot_dir, screenshot_dir_relative_to: "game_dir"},
                 assets: assetcfg,
@@ -225,18 +237,18 @@ async function generateManifest(gameBiz) {
                         jadeite: false,
                         xxmi: true
                     },
-                    preload: formatPreload(packages, "ZenlessZoneZero")
+                    preload: await formatPreload(branches, "ZenlessZoneZero")
                 }
             };
         }
         break;
         case "bh3_global": {
-            let metadatainfo = {versioned_name: `HonkaiImpact 3rd ${packages.game_version} (Global)`, version: packages.game_version, game_hash: ""}
+            let metadatainfo = {versioned_name: `HonkaiImpact 3rd ${branches.main.tag} (Global)`, version: branches.main.tag, game_hash: ""}
             let versioninfo = {
                 metadata: metadatainfo,
                 assets: assetcfg,
                 index_file: "",
-                res_list_url: packages.res_list,
+                res_list_url: "",
                 game: {full: pkg.full_game, diff: pkg.diff_game},
                 audio: {full: pkg.full_audio, diff: pkg.diff_audio}
             };
@@ -250,7 +262,7 @@ async function generateManifest(gameBiz) {
                     gameversions.push(versioninfo);
 
                     data.game_versions.forEach(v => {
-                        if (v.metadata.version !== packages.game_version) {gameversions.push(v);}
+                        if (v.metadata.version !== branches.main.tag) {gameversions.push(v);}
                     });
                 } else {gameversions.push(versioninfo);}
             } else {gameversions.push(versioninfo);}
@@ -259,7 +271,7 @@ async function generateManifest(gameBiz) {
                 version: 1,
                 display_name: "HonkaiImpact 3rd (Global)",
                 biz: "bh3_global",
-                latest_version: packages.game_version,
+                latest_version: branches.main.tag,
                 game_versions: gameversions,
                 paths: {audio_pkg_res_dir: config.audio_pkg_res_dir, exe_filename: config.exe_filename, installation_dir: config.installation_dir, screenshot_dir: config.screenshot_dir, screenshot_dir_relative_to: "game_dir"},
                 assets: assetcfg,
@@ -271,7 +283,7 @@ async function generateManifest(gameBiz) {
                         jadeite: true,
                         xxmi: true
                     },
-                    preload: formatPreload(packages, "HonkaiImpact 3rd")
+                    preload: await formatPreload(branches, "HonkaiImpact 3rd")
                 }
             };
         }
@@ -280,66 +292,148 @@ async function generateManifest(gameBiz) {
     return final;
 }
 
-function formatPackages(packages) {
+async function formatPackages(packages) {
+    let manifest = `https://sg-public-api.hoyoverse.com/downloader/sophon_chunk/api/getBuild?branch=${packages.main.branch}&package_id=${packages.main.package_id}&password=${packages.main.password}&plat_app=${packages.game_id}&tag=${packages.main.tag}`;
+    let rsp = await fetch(`${manifest}`);
+    let r = await rsp.json();
+
     let fg = [];
-    packages.full_game.forEach(e => {
-        return fg.push({
-            file_url: e.url,
-            compressed_size: e.size,
-            decompressed_size: e.decompressed_size,
-            file_hash: e.md5,
+    let fa = [];
+    let dg = [];
+    let da = [];
+
+    if (r.retcode === 0) {
+        let d = r.data;
+
+        let game = d.manifests.filter(e => e.matching_field === "game")[0];
+        let en = d.manifests.filter(e => e.matching_field === "en-us")[0];
+        let cn = d.manifests.filter(e => e.matching_field === "zh-cn")[0];
+        let kr = d.manifests.filter(e => e.matching_field === "ko-kr")[0];
+        let jp = d.manifests.filter(e => e.matching_field === "ja-jp")[0];
+
+        fg.push({
+            file_url: `${game.manifest_download.url_prefix}/${game.manifest.id}`,
+            compressed_size: `${game.stats.compressed_size}`,
+            decompressed_size: `${game.stats.uncompressed_size}`,
+            file_hash: game.manifest.checksum,
             file_path: ""
         });
-    });
 
-    let fa = [];
-    packages.full_audio.forEach(e => {
-        return fa.push({
-            file_url: e.url,
-            compressed_size: e.size,
-            decompressed_size: e.decompressed_size,
-            file_hash: e.md5,
-            language: e.language
+        fa.push({
+            file_url: `${en.manifest_download.url_prefix}/${en.manifest.id}`,
+            compressed_size: `${en.stats.compressed_size}`,
+            decompressed_size: `${en.stats.uncompressed_size}`,
+            file_hash: en.manifest.checksum,
+            language: en.matching_field
         });
-    });
 
-    let dg = [];
-    packages.diffs.forEach(e => {
-        e.game_pkgs.forEach(e2 => {
-            return dg.push({
-                file_url: e2.url,
-                compressed_size: e2.size,
-                decompressed_size: e2.decompressed_size,
-                file_hash: e2.md5,
-                diff_type: "hdiff",
-                original_version: e.version,
-                delete_files: []
-            });
-        })
-    });
+        fa.push({
+            file_url: `${cn.manifest_download.url_prefix}/${cn.manifest.id}`,
+            compressed_size: `${cn.stats.compressed_size}`,
+            decompressed_size: `${cn.stats.uncompressed_size}`,
+            file_hash: cn.manifest.checksum,
+            language: cn.matching_field
+        });
 
-    let da = [];
-    packages.diffs.forEach(e => {
-        e.audio_pkgs.forEach(e2 => {
-            return da.push({
-                file_url: e2.url,
-                compressed_size: e2.size,
-                decompressed_size: e2.decompressed_size,
-                file_hash: e2.md5,
-                diff_type: "ldiff",
-                original_version: e.version,
-                language: e2.language
-            });
-        })
-    });
+        fa.push({
+            file_url: `${kr.manifest_download.url_prefix}/${kr.manifest.id}`,
+            compressed_size: `${kr.stats.compressed_size}`,
+            decompressed_size: `${kr.stats.uncompressed_size}`,
+            file_hash: kr.manifest.checksum,
+            language: kr.matching_field
+        });
 
-    return {full_game: fg, full_audio: fa, diff_game: dg, diff_audio: da};
+        fa.push({
+            file_url: `${jp.manifest_download.url_prefix}/${jp.manifest.id}`,
+            compressed_size: `${jp.stats.compressed_size}`,
+            decompressed_size: `${jp.stats.uncompressed_size}`,
+            file_hash: jp.manifest.checksum,
+            language: jp.matching_field
+        });
+
+        return {full_game: fg, full_audio: fa, diff_game: dg, diff_audio: da, manifest_base: game.manifest_download.url_prefix, chunk_base: game.chunk_download.url_prefix};
+    }
 }
 
-function formatPreload(pkgs, name) {
+async function formatPreload(pkgs, name) {
     let preloaddata = {};
 
-    if (pkgs.preload.major !== null) {
+    if (pkgs.preload !== null) {
+        let manifest = `https://sg-public-api.hoyoverse.com/downloader/sophon_chunk/api/getBuild?branch=${pkgs.preload.branch}&package_id=${pkgs.preload.package_id}&password=${pkgs.preload.password}&plat_app=${pkgs.game_id}`;
+        let rsp = await fetch(`${manifest}`);
+        let r = await rsp.json();
+
+        let pfg = [];
+        let pfa = [];
+        let pdg = [];
+        let pda = [];
+
+        if (r.retcode === 0) {
+            let d = r.data;
+
+            let game = d.manifests.filter(e => e.matching_field === "game")[0];
+            let en = d.manifests.filter(e => e.matching_field === "en-us")[0];
+            let cn = d.manifests.filter(e => e.matching_field === "zh-cn")[0];
+            let kr = d.manifests.filter(e => e.matching_field === "ko-kr")[0];
+            let jp = d.manifests.filter(e => e.matching_field === "ja-jp")[0];
+
+            pfg.push({
+                file_url: `${game.manifest_download.url_prefix}/${game.manifest.id}`,
+                compressed_size: `${game.stats.compressed_size}`,
+                decompressed_size: `${game.stats.uncompressed_size}`,
+                file_hash: game.manifest.checksum,
+                file_path: ""
+            });
+
+            pfa.push({
+                file_url: `${en.manifest_download.url_prefix}/${en.manifest.id}`,
+                compressed_size: `${en.stats.compressed_size}`,
+                decompressed_size: `${en.stats.uncompressed_size}`,
+                file_hash: en.manifest.checksum,
+                language: en.matching_field
+            });
+
+            pfa.push({
+                file_url: `${cn.manifest_download.url_prefix}/${cn.manifest.id}`,
+                compressed_size: `${cn.stats.compressed_size}`,
+                decompressed_size: `${cn.stats.uncompressed_size}`,
+                file_hash: cn.manifest.checksum,
+                language: cn.matching_field
+            });
+
+            pfa.push({
+                file_url: `${kr.manifest_download.url_prefix}/${kr.manifest.id}`,
+                compressed_size: `${kr.stats.compressed_size}`,
+                decompressed_size: `${kr.stats.uncompressed_size}`,
+                file_hash: kr.manifest.checksum,
+                language: kr.matching_field
+            });
+
+            pfa.push({
+                file_url: `${jp.manifest_download.url_prefix}/${jp.manifest.id}`,
+                compressed_size: `${jp.stats.compressed_size}`,
+                decompressed_size: `${jp.stats.uncompressed_size}`,
+                file_hash: jp.manifest.checksum,
+                language: jp.matching_field
+            });
+
+            let pmetadatainfo = {
+                versioned_name: `${name} ${pkgs.preload.tag} Preload (Global)`,
+                version: pkgs.preload.tag,
+                game_hash: "",
+            }
+
+            preloaddata = {
+                metadata: pmetadatainfo,
+                index_file: `${game.manifest_download.url_prefix}`,
+                res_list_url: `${game.chunk_download.url_prefix}`,
+                game: {full: pfg, diff: pdg},
+                audio: {full: pfa, diff: pda}
+            }
+        }
+    }
+
+    /*if (pkgs.preload.major !== null) {
         let pfg = [];
         pkgs.preload.major.game_pkgs.forEach(e => {
             return pfg.push({
@@ -405,13 +499,12 @@ function formatPreload(pkgs, name) {
             game: {full: pfg, diff: pdg},
             audio: {full: pfa, diff: pda}
         }
-    }
+    }*/
 
     return preloaddata;
 }
 
-// Deperacate generation of hk4e and nap as they are now sophon only
-//generateManifest("hk4e_global").then(r => writeFileSync(gipath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
-generateManifest("hkrpg_global").then(r => writeFileSync(hsrpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
-//generateManifest("nap_global").then(r => writeFileSync(zzzpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
-generateManifest("bh3_global").then(r => writeFileSync(bhpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
+generateManifest("hk4e_global").then(r => writeFileSync(gipath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
+//generateManifest("hkrpg_global").then(r => writeFileSync(hsrpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
+generateManifest("nap_global").then(r => writeFileSync(zzzpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
+//generateManifest("bh3_global").then(r => writeFileSync(bhpath, JSON.stringify(r, null, 2), {encoding: "utf8"}));
